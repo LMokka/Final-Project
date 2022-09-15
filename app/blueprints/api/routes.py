e1b553cf0b90e2d66845870c5bcde43b1925e2e4
LMokka <87783898+LMokka@users.noreply.github.com>
2022-09-15 16:10:13 -0500

    first commit

---
+++ app/blueprints/api/routes.py
@@ -0,0 +1,80 @@
+from . import api
+from .auth import basic_auth, token_auth
+from flask import jsonify, request
+from app.models import Post, User
+
+
+@api.route('/token')
+@basic_auth.login_required
+def get_token():
+    user = basic_auth.current_user()
+    token = user.get_token()
+    return jsonify({'token': token, 'token_expiration': user.token_expiration})
+
+
+@api.route('/posts', methods=["GET"])
+def get_posts():
+    posts = Post.query.all()
+    return jsonify([p.to_dict() for p in posts])
+
+
+@api.route('/posts/<post_id>')
+def get_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return jsonify(post.to_dict())
+
+
+@api.route('/posts', methods=["POST"])
+@token_auth.login_required
+def create_post():
+    if not request.is_json:
+        return jsonify({'error': 'Your request content-type must be application/json'}), 400
+    # Get the data from the request body
+    data = request.json
+    print(data, type(data))
+    # Validate the data
+    for field in ['title', 'body']:
+        if field not in data:
+            # if field not in request body, respond with a 400 error
+            return jsonify({'error': f"'{field}' must be in request body"}), 400
+    
+    # Get fields from data dict
+    title = data.get('title')
+    body = data.get('body')
+    user = token_auth.current_user()
+    user_id = user.id
+    # Create new instance of post with data
+    new_post = Post(title=title, body=body, user_id=user_id)
+    return jsonify(new_post.to_dict()), 201
+
+
+# Get a user by user_id
+@api.route('/users/<user_id>')
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify(user.to_dict())
+
+
+# Create a new user
+@api.route('/users', methods=["POST"])
+def create_user():
+    # Check to make sure there is a JSON body on the request
+    if not request.is_json:
+        return jsonify({"error": "Your request content-type must be application/json"}), 400
+    # Get data from request
+    data = request.json
+    # Make sure data has all of the required fields
+    for field in ['email', 'username', 'password']:
+        if field not in data:
+            return jsonify({"error": f"'{field}' must be in request body"}), 400
+    # Getting the data from the dictionary
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+    # Before we add the user to the database, check to see if there is already a user with username or email
+    existing_user = User.query.filter((User.email == email) | (User.username == username)).first()
+    if existing_user:
+        return jsonify({"error": "User with username and/or email already exists"}), 400
+    # Create new user with request data
+    new_user = User(email=email, username=username, password=password)
+    return jsonify(new_user.to_dict()), 201
---
+++ app/models.py
@@ -0,0 +1,96 @@
+import base64
+import os
+from app import db, login
+from datetime import datetime, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Save the password as the hashed version of the password
+        self.set_password(kwargs['password'])
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<User {self.id} | {self.username}>"
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        db.session.commit()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "username": self.username,
+            "date_created": self.date_created,
+            "posts": [p.to_dict() for p in self.posts.all()]
+        }
+
+    def get_token(self, expires_in=300):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.commit()
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+        db.session.commit()
+        
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # SQL equivalent = FOREIGN KEY(user_id) REFERENCES user(id)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<Post {self.id} | {self.title}>"
+
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            if key in {'title', 'body'}:
+                setattr(self, key, value)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body,
+            "date_created": self.date_created,
+            "user_id": self.user_id
+        }
